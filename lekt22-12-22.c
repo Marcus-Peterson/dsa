141513855b346c81946c45f82e809df434913080
Marcus Peterson <73422974+Marcus-Peterson@users.noreply.github.com>
2022-12-22 12:48:38 +0000

    hejsan

---
+++ lekt22-12-22.c
@@ -0,0 +1,13 @@
+//Linked lists 
+//3 node -> 7 node -> 23 node -> 45
+
+int main(){
+    int a[] = {2,3,5};
+    int b[] = {6,2,3};
+
+    int *pa = &a, *pb = &b;
+    int *arr[] = {pa,pb};
+
+return 0;
+}
+
